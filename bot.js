02c86e39429888cd01b8747ca603cfd7bb9beb4c
muakasan <muakasan@gmail.com>
2016-06-22 16:04:34 -0400

    added async queue

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ http://tex.stackexchange.com/questions/184537/bad-math-environment-delimiter-in-
 http://stackoverflow.com/questions/6605006/convert-pdf-to-image-with-high-resolution
 http://stackoverflow.com/questions/3482901/is-it-possible-to-compile-a-latex-document-via-node-js
 */
+
 require('dotenv').config();
 
 var login = require("facebook-chat-api");
@@ -15,8 +16,28 @@ var async = require('async');
 
 login({email: process.env.FB_EMAIL, password: process.env.FB_PASSWORD}, function callback (err, api) {
     if(err) return console.error(err);
+		var q = async.queue(function (task, cb) {
+		    fs.writeFile("temp-files/temp.tex", task.latexCode, function(err) {
+				if(err) {
+					return console.log(err);
+				}
+			});
 
-    api.listen(function callback(err, messageRecd) {
+			var pdflatex = spawn('pdflatex', ['-output-directory', 'temp-files/', 'temp-files/temp.tex']);
+			pdflatex.on('exit', function(code) {					
+				console.log('pdflatex exited with code ' + code);
+				var convert = spawn('convert', ['-density', '300', '-quality', '100', 'temp-files/temp.pdf', 'temp-files/equation.jpg']);
+				convert.on('exit', function(code) {
+					console.log('convert exited with code ' + code);
+					var msgToSend = 	{ body: task.equationText,
+										attachment: fs.createReadStream("temp-files/equation.jpg")};
+						api.sendMessage(msgToSend, task.threadID);
+						cb();
+
+				});
+			});	
+		});     
+		api.listen(function callback(err, messageRecd) {
 		if(messageRecd.body)
 		{
 			var matches = messageRecd.body.match(/^\$(.*)\$$/);
@@ -29,24 +50,11 @@ login({email: process.env.FB_EMAIL, password: process.env.FB_PASSWORD}, function
 					if(err) {
 						return console.log(err);
 					}
-					console.log(Mustache.render(template, view));
-					fs.writeFile("temp-files/temp.tex", Mustache.render(template, view), function(err) {
-						if(err) {
-							return console.log(err);
-						}
-					});
-
-					var pdflatex = spawn('pdflatex', ['-output-directory', 'temp-files/', 'temp-files/temp.tex']);
-					pdflatex.on('exit', function(code) {					
-						console.log('pdflatex exited with code ' + code);
-						var convert = spawn('convert', ['-density', '300', '-quality', '100', 'temp-files/temp.pdf', 'temp-files/equation.jpg']);
-						convert.on('exit', function(code) {
-							console.log('convert exited with code ' + code);
-							var msgToSend = 	{ body: view.equationText,
-												attachment: fs.createReadStream("temp-files/equation.jpg")};
-								api.sendMessage(msgToSend, messageRecd.threadID);
-						});
-					});	
+					var latexCode = Mustache.render(template, view);
+					console.log(latexCode);
+					q.push({	threadID: messageRecd.threadID, 
+								equationText: view.equationText, 
+								latexCode: latexCode});					
 				});	
 			}
 		}
